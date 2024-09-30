23ee895e531254b799ddeeef3acdb0ace92b3ed5
ayush931 <ayushkumar9315983@gmail.com>
2024-09-30 22:11:51 +0530

    All setup Completed

--- src/app.js
+++ src/app.js
@@ -14,5 +14,13 @@ app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter) // http://localhost:3000/api/vi/users/register
+
 export { app }
 
