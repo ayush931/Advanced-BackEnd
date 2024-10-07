228aa96c438fd08553e1ea951d6aea25715bcb0d
ayush931 <ayushkumar9315983@gmail.com>
2024-10-07 09:57:54 +0530

    Added todos

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -312,6 +312,8 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
         throw new ApiError(400, 'Cover image file is missing')
     }
 
+    // TODO: delete old cover image.
+
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!coverImage.url) {
